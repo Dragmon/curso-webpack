4d922f6efc249e4e85b053d1243047df2d54a511
Carlos Perez <carlos@ioncom.com.mx>
2017-12-21 19:34:07 -0600

    manejo de multiple entrypoints clase 10

---
+++ multiples-entrypoints/src/js/contactos.js
@@ -0,0 +1,4 @@
+import '../css/estilos.css'
+//console.log("Hola, este es un archivo de webpack");
+document.write('Hola a todos desde contactos');
+console.log("Hola, desde contactos este es un archivo de webpack.config.js dentro multiples entrypoints");
---
+++ multiples-entrypoints/src/js/index.js
@@ -0,0 +1,4 @@
+import '../css/estilos.css'
+//console.log("Hola, este es un archivo de webpack");
+document.write('Hola a todos desde home');
+console.log("Hola, este es un archivo de webpack.config.js dentro de multiples entrypoints");
