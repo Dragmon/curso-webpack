d0a722a5f330ed7a30e7bfdafc6f1bd2a9c44798
Carlos Perez <carlos@ioncom.com.mx>
2017-12-22 17:11:51 -0600

    uso de babel calse 12

---
+++ babel-loader/index.js
@@ -0,0 +1,5 @@
+import './estilos.css';
+import {firstMessage} from './message.js';
+//console.log("Hola, este es un archivo de webpack");
+document.write(firstMessage);
+console.log("Hola, este es un archivo con babel");
