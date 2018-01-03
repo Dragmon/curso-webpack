ae640eb591ce8e9d0e4dc0961848e84428b16ac1
Carlos Perez <carlos@ioncom.com.mx>
2018-01-02 19:20:23 -0600

    uso de archivos json clase 17

---
+++ json-loader/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const extractTextPlugin = require ('extract-text-webpack-plugin');
+
+module.exports = {
+  // __dirname se basa en el directorio actual, este cambiara dependiendo de donde se encuentre el archivo
+  // webpack.config.js
+  entry:path.resolve(__dirname, 'src/js/index.js'),
+  output:{
+    path: path.resolve(__dirname, 'dist'),
+    filename:'bundle.js'
+  },
+  module:{
+    rules:[
+      //Aquí van los loader
+      {
+        //test: tipo de archivo a reconocer (usando expreciones regualares),
+        //use: que loader se va a encargara del archuivo
+        test: /\.js$/,
+        use:{
+          loader: 'babel-loader',
+          options:{
+            presets: ['es2015']
+          }
+        }
+      },
+      {
+        test: /\.json$/,
+        use:'json-loader',
+      },
+      {
+        test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/,
+        use:[
+          {
+            loader: 'url-loader',
+            options: {
+              limit:120000
+            }
+          }
+        ]
+      },
+      {
+        //test: tipo de archivo a reconocer (usando expreciones regualares),
+        //use: que loader se va a encargara del archuivo
+        test: /\.css$/,
+        use:extractTextPlugin.extract({
+          //['style-loader', 'css-loader']
+          use:"css-loader"
+        }),
+      }
+    ]
+  },
+  plugins:[
+    //plugins instalados
+    new extractTextPlugin("css/[name].css")
+  ]
+}
